347e9422f627f0113c9f11ed34aa5037397f3068
Kaival Patel <39383435+Kaival-Patel@users.noreply.github.com>
2022-05-26 08:43:35 +0530

    Started Book section

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const APIBASEURL = "http://localhost:3000/api/v1";
-
+export const APIBOOKCOVERURL= "http://localhost:3000/public/book_cover";
 export const validateApiResponse = (response) => {
     console.info(response);
     if(response.s!=1){
--- src/screens/books/books_section.js
+++ src/screens/books/books_section.js
@@ -1,16 +1,52 @@
-import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux';
-import { getBooks } from '../../services/book_services'
+import { Card, Col, List, Row } from "antd";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { APIBOOKCOVERURL } from "../../constants";
+import { getBooks } from "../../services/book_services";
+import "./books_section.css";
 
-function BookSection({booksList}) {
-  console.warn(booksList);
+function BookCard({ book }) {
   return (
-    <div>
-      <h1>
-        {booksList.length>0? booksList[0].name : "No Books"}
-      </h1>
+    <div className="bookCard">
+      <img className="bookCover" src={APIBOOKCOVERURL + "/" + book.cover} />
+      <div className="bookContent">
+        <div className="name">
+          {book.name}
+        </div>
+        <p className="author">
+          {book.author}
+        </p>
+        <div className="footer">
+          <div className="footer_content">
+            {book.pages}
+          </div>
+          <div className="footer_title">
+            Pages
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default BookSection
+function BookSection({ booksList }) {
+  return (
+    <div>
+      <h3 className="title">For you</h3>
+      <Row>
+        {booksList.map((item) => (
+          <Col xs={24} sm={12}>
+            {BookCard({book:item})}
+          </Col>
+        ))}
+      </Row>
+      {/* <List
+        grid={{ gutter: 16, xs: 1, sm: 2, md: 4, lg: 6, xl: 8, xxl: 10 }}
+        dataSource={booksList}
+        renderItem={(item) => <Card>{item.name}</Card>}
+      /> */}
+    </div>
+  );
+}
+
+export default BookSection;
