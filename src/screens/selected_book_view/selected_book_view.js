58b53931cdcc8b7877f4b00263b85cce3580a8eb
Kaival Patel <39383435+Kaival-Patel@users.noreply.github.com>
2022-05-27 08:51:48 +0530

    Selected Book View started

---
+++ src/screens/selected_book_view/selected_book_view.js
@@ -0,0 +1,16 @@
+import { Col } from 'antd'
+import React, { useEffect } from 'react'
+import './selected_book_view.css';
+function SelectedBookView() {
+//   const state = use  
+  return (
+    <div className="about_book">
+        <h3 className='header_title'>
+            About the book
+        </h3>
+        {/* <img src='' */}
+      </div>
+  )
+}
+
+export default SelectedBookView
